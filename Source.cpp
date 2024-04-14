0c18872a1c0bbce8bbfcd73a1979e21f2d0ccad0
slothyy12 <126151422+slothyy12@users.noreply.github.com>
2024-04-15 00:59:54 +0300

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,12 @@
+/**	\file Target Number game
+* \brief This is a game in which the user's goal is to turn all the values in the 3x3 grid to 9s.
+* \details The game starts with an initial configuration of digits and the user’s target is to change all of them to 9 in the minimum number of
+moves
+* \author Tamanda Mdyanyama
+* \date 12/04/2024
+*/
+
+#include <iostream>
+using namespace std
+
+int main()
